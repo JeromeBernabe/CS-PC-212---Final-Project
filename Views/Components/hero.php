3baba4d3807c6bd55baf7e6ba8c91461a1dac939
unknown <jerbz1122@gmail.com>
2024-11-25 22:14:06 +0800

    Revised Filesystem

---
+++ Views/Components/hero.php
@@ -0,0 +1,6 @@
+<div class = 'container' id = 'Feature'>
+    Feature
+</div>
+<div class = 'container' id = 'Pre-Order'>
+    Pre-Order
+</div>
---
+++ Views/Components/navbar.php
@@ -0,0 +1,8 @@
+<div id = 'navbar'>
+    <div class = 'button 'id = 'logo'>Logo</div>
+    <div class = 'section'>
+        <div class = "button" id = 'Shop'>Shop</div>
+        <div class = "button">Button</div>
+        <div class = "button">Button</div>
+    </div>
+</div>
